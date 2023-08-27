583cb97861bd5161ac8f0400526211e5368e203a
Andre Visser <chrisjanv@gmail.com>
2023-08-27 14:48:16 +0200

    database connection established

---
+++ src/php/config.php
@@ -0,0 +1,15 @@
+<?php
+// Database configuration
+$host = "localhost";
+$dbname = "dre_stays";
+$username = "root";
+
+// Establish a database connection using PDO
+try {
+    $db = new PDO("mysql:host=$host;dbname=$dbname", $username);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo("Database connection established");
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
+?>
